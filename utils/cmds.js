a635853ca034871ee6398bd0c7f7014117951245
aruppert <aruppert@pm.me>
2019-12-02 11:38:57 +0100

    Add cmd module

---
+++ utils/cmds.js
@@ -0,0 +1,25 @@
+const passwords = {
+  wifi: 123,
+  mac: "mac123"
+};
+
+function get(key) {
+  return passwords[key];
+}
+
+function set(key, value) {
+  passwordsl[key] = value;
+}
+
+function unset(key) {
+  delete passwords[key];
+}
+// module.exports = {
+//   get,
+//   set,
+unset;
+// }
+
+exports.get = get;
+exports.set = set;
+exports.unset = unset;
